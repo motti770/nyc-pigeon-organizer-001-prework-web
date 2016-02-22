d209bc4a797331c3feae74260b6c5a70a4d15d62
motti770 <mottigottlieb@gmail.com>
2016-02-22 23:02:32 +0000

    nitrous-backup at Mon Feb 22 23:02:32 UTC 2016

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each do |a, b|
+    hash[a] = b
+
 end
+end
+# pigeon_data = {
+  # :color => {
+    #:purple => ["Theo", "Peter Jr.", "Lucky"],
+    #:grey => ["Theo", "Peter Jr.", "Ms. K"],
+    #:white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+   # :brown => ["Queenie", "Alex"]
+  #}
